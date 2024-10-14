1294a3148ec5968b94677f42c684e72016ac5a84
Harry <oyjunhan@hotmail.com>
2024-10-14 13:38:25 -0700

    Implement adding functions

--- Deque.java
+++ Deque.java
@@ -1,19 +1,58 @@
 public class Deque<Item> implements Iterable<Item> {
+    private Node<Item> first;
+    private Node<Item> last;
+    private int n;
+
+    private static class Node<Item> {
+        private Item item;
+        private Node<Item> next;
+        private Node<Item> previous;
+    }
 
     // construct an empty deque
-    public Deque() {}
+    public Deque() {
+        first = null;
+        last = null;
+        n = 0;
+    }
 
     // is the deque empty?
-    public boolean isEmpty() {}
+    public boolean isEmpty() {
+        return first == null;
+    }
 
     // return the number of items on the deque
-    public int size() {}
+    public int size() {
+        return n;
+    }
 
     // add the item to the front
-    public void addFirst(Item item) {}
+    public void addFirst(Item item) {
+        Node<Item> oldFirst = first;
+        first = new Node<Item>();
+        first.item = item;
+        first.previous = null;
+        if (isEmpty()) {
+            last = first;
+        } else {
+            oldFirst.previous = first;
+        }
+        n++;
+    }
 
     // add the item to the back
-    public void addLast(Item item) {}
+    public void addLast(Item item) {
+        Node<Item> oldlast = last;
+        last = new Node<Item>();
+        last.item = item;
+        last.next = null;
+        if (isEmpty()) {
+            first = last;
+        } else {
+            oldlast.next = last;
+        }
+        n++;
+    }
 
     // remove and return the item from the front
     public Item removeFirst() {}
