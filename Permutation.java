f8160a6f2db0c0f4dca4a7295cd5fbe2e95154fa
Harry <oyjunhan@hotmail.com>
2024-10-15 17:05:16 -0700

    Try parsing integer directly from args

--- Permutation.java
+++ Permutation.java
@@ -8,7 +8,7 @@ import edu.princeton.cs.algs4.StdIn;
 public class Permutation {
     public static void main(String[] args) {
         RandomizedQueue<String> list = new RandomizedQueue<>();
-        int k = StdIn.readInt();
+        int k = Integer.parseInt(args[0]);
         while (!StdIn.isEmpty()) {
             list.enqueue(StdIn.readString());
         }
